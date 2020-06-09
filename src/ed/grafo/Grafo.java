e43d127a5135dd7a788a3e06bef8dccc67b6cf98
Leandro Favio Gomez Racca <leo1ragomez95@gmail.com>
2020-06-09 17:56:20 +0200

    addNodo +comprobar duplicados, addEnlace implementado

--- src/ed/grafo/Grafo.java
+++ src/ed/grafo/Grafo.java
@@ -35,7 +35,25 @@ public class Grafo<T1, T2> {
     }
 
     public void addNodo(int id, T1 info) {
-        listaNodos.set(id, new Nodo(id, info));
+        if (listaNodos.get(id) == null) {
+            listaNodos.set(id, new Nodo(id, info));
+        }
+        else System.out.println("Nodo repetido. ID = " + id);
+    }
+
+    public void addEnlace(int nodoA, int nodoB, T2 peso) {
+        Enlace temp = new Enlace(nodoA, nodoB, peso);
+        Enlace tempInverso = new Enlace(nodoB, nodoA, peso);
+        if (listaNodos.get(nodoA).listaDeEnlaces.contains(temp)) {
+            System.out.println("Enlace ya existente. N1 =" + nodoA + "N2 =" + nodoB);
+        }
+        else if (listaNodos.get(nodoA).listaDeEnlaces.contains(tempInverso)){
+            System.out.println("Enlace ya existente. N1 =" + nodoB + "N2 =" + nodoA);
+        }
+        else {
+            listaNodos.get(nodoA).listaDeEnlaces.add(temp);
+            listaNodos.get(nodoB).listaDeEnlaces.add(temp);
+        }
     }
 
 
