5caf2a24d6395b5e00e4d34de326f2f2a57da8a6
Leandro Favio Gomez Racca <leo1ragomez95@gmail.com>
2020-06-09 15:37:37 +0200

    Estructura basica del grafo, Nodo y Enlace

--- src/ed/grafo/Grafo.java
+++ src/ed/grafo/Grafo.java
@@ -1,4 +1,38 @@
 package ed.grafo;
 
-public class Grafo {
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Grafo<T1, T2> {
+    private ArrayList<Nodo> listaNodos;
+
+    public  Grafo(int nNodos) {
+        this.listaNodos = new ArrayList<>(nNodos);
+    }
+
+    private class Nodo {
+        private int id;
+        private T1 info;
+        private LinkedList<Enlace> listaDeEnlaces;
+
+        public Nodo(int id, T1 info) {
+            this.id = id;
+            this.info = info;
+            this.listaDeEnlaces = new LinkedList<>();
+        }
+    }
+
+    private class Enlace {
+        int nodoA;
+        int nodoB;
+        T2 peso;
+
+        public Enlace(int nodoA, int nodoB, T2 peso) {
+            this.nodoA = nodoA;
+            this.nodoB = nodoB;
+            this.peso = peso;
+        }
+    }
+
+
 }
